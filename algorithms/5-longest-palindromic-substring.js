e8b65879e08dfcd75ed271ed7d68f0df940784e5
Ken Huang <bluetch@gmail.com>
2022-08-18 08:55:18 +0800

    5-longest-palindromic-substring

--- algorithms/5-longest-palindromic-substring.js
+++ algorithms/5-longest-palindromic-substring.js
@@ -1,11 +1,37 @@
+
 /**
- * Problem: https://leetcode.com/problems/longest-palindromic-substring/
+ * Problem: https://leetcode.com/problems/longest-palindromic-substring
  * Difficulty: Medium
+ * Language: JavaScript
+ * Accepted date: 2017-03-07
+ * 
+ * 
  * @param {string} s
  * @return {string}
  */
+ const longestPalindrome = (s) => {
+  const expandAroundCenter = (s, left, right) => {
+      let L = left,
+          R = right;
+      while (L >= 0 && R < s.length && s.charAt(L) == s.charAt(R)) {
+          L--;
+          R++;
+      }
+      return parseInt(R - L - 1);
+  }
 
-// Manacher
-const longestPalindrome = (s) => {
-    
-};
+  let start = 0,
+      end = 0;
+  for (let i = 0; i < s.length; i++) {
+      let len1 = expandAroundCenter(s, i, i);
+      let len2 = expandAroundCenter(s, i, i + 1);
+      let len = Math.max(len1, len2);
+      // console.log("i="+i, "len="+len,"start="+start, "end="+end);
+      if (len > (end - start)) {
+          start = parseInt(i - parseInt((len - 1) / 2));
+          end = parseInt(i + len / 2);
+      }
+      // console.log("===================", "start="+start, "end="+end);
+  }
+  return s.substring(start, end + 1);
+}
