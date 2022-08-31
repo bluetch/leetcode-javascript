82cdf4ab8910baa2a298cd0ce7b9647f244c1605
Ken Huang <bluetch@gmail.com>
2022-08-31 10:34:37 +0800

    19 remove nth node from end of list

---
+++ algorithms/19-remove-nth-node-from-end-of-list.js
@@ -0,0 +1,18 @@
+
+
+/**
+ * Problem: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ * Difficulty: Medium
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+
+const removeNthFromEnd = (head, n) => {
+
+};
---
+++ algorithms/binary-search.js
@@ -0,0 +1,21 @@
+const binarySearch = (arr, num) => {
+  // arr.sort((a, b) => (a - b));
+  let min = 0,
+    max = arr.length - 1;
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    if (num > arr[middle]) {
+      min = middle + 1;
+    }
+    else if (num < arr[middle]) {
+      max = middle - 1;
+    }
+    else {
+      console.log(arr);
+      console.log("the number: " + num + " is at " + middle);
+      return middle;
+    }
+  }
+  console.log("cannot found the number: " + num);
+  return -1;
+}
